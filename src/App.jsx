3db3a7ed31958e0079353fc3fd8774328f954cef
anyefremova <anna.efremova.02@yandex.ru>
2023-11-27 03:47:10 +0500

    added css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import './App.scss'
-import { Tariff } from './Tariff/Tariff.jsx'
-import BlueColor from './Tariff/tariff-blue.module.scss'
+import Tariff from './Tariff/Tariff.jsx'
+import styles from './Tariff.module.scss';
 
 const tariffs = [
-  { id: 1, price: 300, speed: 10, color: BlueColor },
-  { id: 2, price: 450, speed: 50 },
-  { id: 3, price: 550, speed: 100 },
-  { id: 2, price: 1000, speed: 200 }
+  { id: 1, title: 300, price: 300, speed: 10, color: '#87CEEB' },
+  { id: 2, title: 450, price: 450, speed: 50, color: '#90EE90' },
+  { id: 3, title: 550, price: 550, speed: 100, color: '#FF0000' },
+  { id: 2, title: 1000, price: 1000, speed: 200, color: '#808080' }
 ]
 
 function App() {
   return (
-    <div className="App">
-      {tariffs.map((tariff) => (
+    <div className={styles.cardList}>
+      {tariffs.map((tariff, index) => (
         <Tariff
-          key={tariff.id}
+          key={index}
+          name={tariff.title}
           price={tariff.price}
           speed={tariff.speed}
+          details={tariff.details}
+          isHighlighted={index === 2}
           color={tariff.color}
         />
       ))}
--- src/Tariff/Tariff.jsx
+++ src/Tariff/Tariff.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import styles from './Tariff.module.scss';
 
 function Tariff(props) {
-    const { price, speed, color } = props;
+    const { title, price, speed, isHighlighted, cardColor } = props;
+
+    const cardStyle = {
+        width: isHighlighted ? '300px' : '200px',
+        height: isHighlighted ? '350px' : '300px',
+    };
 
     return (
-        <div className="tariff">
-            <div className={color}>
-                <h3 className="title">Безлимитный {price}</h3>
-                <p className="price">руб {price} /мес</p>
-                <p className="speed">до {speed} Мбит/сек</p>
-                <p className="details">Объем включенного тарифа не ограничен</p>
-            </div>
+        <div className={`${styles.tariffCard} ${isHighlighted ? styles.highlighted : ''}`} style={cardColor}>
+            <h3 className={styles.title}>Безлимитный {title}</h3>
+            <p className={styles.price}>руб {price} /мес</p>
+            <p className="speed">до {speed} Мбит/сек</p>
+            <p className="details">Объем включенного тарифа не ограничен</p>
         </div>
     )
 }
 
-export { Tariff }
+export default Tariff
