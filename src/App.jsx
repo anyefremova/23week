ea12851337d6f87d35ad4c4144185715d9f24a91
anyefremova <anna.efremova.02@yandex.ru>
2023-11-28 16:19:30 +0500

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import './App.scss'
-import { Tariff } from './Tariff/Tariff.jsx'
-import BlueColor from './Tariff/tariff-blue.module.scss'
+import './App.scss';
+import Tariff from './Tariff/Tariff.jsx';
+import './Tariff/Tariff.module.scss';
 
 const tariffs = [
-  { id: 1, price: 300, speed: 10, color: BlueColor },
-  { id: 2, price: 450, speed: 50 },
-  { id: 3, price: 550, speed: 100 },
-  { id: 2, price: 1000, speed: 200 }
-]
+  { id: 1, title: 300, price: 300, speed: 10, color: "red" },
+  { id: 2, title: 450, price: 450, speed: 50, color: "green" },
+  { id: 3, title: 550, price: 550, speed: 100, color: "blue" },
+  { id: 4, title: 1000, price: 1000, speed: 200, color: "gray" }
+];
 
 function App() {
   return (
     <div className={styles.cardList}>
       {tariffs.map((tariff, index) => (
         <Tariff
-          color={tariff.color}
           key={tariff.id}
+          title={tariff.title}
           price={tariff.price}
           speed={tariff.speed}
+          color={tariff.color}
         />
       ))}
     </div>
