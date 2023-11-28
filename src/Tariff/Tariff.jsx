f7796e13826ea4e843144f5df4ba878c5ce0f8b3
anyefremova <anna.efremova.02@yandex.ru>
2023-11-28 16:11:49 +0500

    added a class for color

--- src/Tariff/Tariff.jsx
+++ src/Tariff/Tariff.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './Tariff.module.scss';
 
 function Tariff(props) {
-    const { price, speed, color } = props;
+    const { title, price, speed, color } = props;
 
     return (
-        <div className="tariff">
-            <div className={color}>
-                <h3 className="title">Безлимитный {price}</h3>
-                <p className="price">руб {price} /мес</p>
-                <p className="speed">до {speed} Мбит/сек</p>
-                <p className="details">Объем включенного тарифа не ограничен</p>
-            </div>
+        <div className={color}>
+            <h3 className={title}>Безлимитный {title}</h3>
+            <p className={price}>руб {price} /мес</p>
+            <p className="speed">до {speed} Мбит/сек</p>
+            <p className="details">Объем включенного тарифа не ограничен</p>
         </div>
     )
 }
 
-export { Tariff }
+export default Tariff
