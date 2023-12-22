90063c1902c7ef13d4e6ad3a1f91897b818d4020
anyefremova <anna.efremova.02@yandex.ru>
2023-12-23 02:59:00 +0500

    added styles to selected

--- src/Tariff/Tariff.jsx
+++ src/Tariff/Tariff.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import './Tariff.scss';
 
 function Tariff(props) {
-    const { price, speed, color } = props;
+    const { price, speed, color, select } = props;
     const tariffClass = `tariff ${color} ${select ? 'selected' : ''}`;
 
     return (
         <div className={tariffClass}>
-            <div className="header">
-                <h3 className="title">Безлимитный {price}</h3>
-            </div>
+            <h3 className="title">Безлимитный {price}</h3>
             <p className="price">руб {price} /мес</p>
             <p className="speed">до {speed} Мбит/сек</p>
             <p className="details">Объем включенного тарифа не ограничен</p>
