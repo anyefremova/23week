47d581757c6d38ec34b798e3f3d9a2b196ccc08b
anyefremova <anna.efremova.02@yandex.ru>
2023-12-22 19:37:53 +0500

    added select

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Tariff from './Tariff/Tariff.jsx';
 import './Tariff/Tariff.scss';
 
 const tariffs = [
-  { id: 1, title: 300, price: 300, speed: 10, color: "blue" },
-  { id: 2, title: 450, price: 450, speed: 50, color: "green" },
-  { id: 3, title: 550, price: 550, speed: 100, color: "red" },
-  { id: 4, title: 1000, price: 1000, speed: 200, color: "black" }
+  { id: 1, title: 300, price: 300, speed: 10, color: "blue", select: false },
+  { id: 2, title: 450, price: 450, speed: 50, color: "green", select: false },
+  { id: 3, title: 550, price: 550, speed: 100, color: "red", select: true },
+  { id: 4, title: 1000, price: 1000, speed: 200, color: "black", select: false }
 ];
 
 function App() {
@@ -19,6 +19,7 @@ function App() {
           price={tariff.price}
           speed={tariff.speed}
           color={tariff.color}
+          select={tariff.select}
         />
       ))}
     </div>
--- src/Tariff/Tariff.jsx
+++ src/Tariff/Tariff.jsx
@@ -3,18 +3,17 @@ import './Tariff.scss';
 
 function Tariff(props) {
     const { price, speed, color } = props;
+    const tariffClass = `tariff ${color} ${select ? 'selected' : ''}`;
 
     return (
-        <div className="tariff">
-            <div className={color}>
+        <div className={tariffClass}>
+            <div className="header">
                 <h3 className="title">Безлимитный {price}</h3>
             </div>
-            <div className={color}>
-                <p className="price">руб {price} /мес</p>
-            </div>
+            <p className="price">руб {price} /мес</p>
             <p className="speed">до {speed} Мбит/сек</p>
             <p className="details">Объем включенного тарифа не ограничен</p>
-        </div>
+        </div >
     )
 }
 
