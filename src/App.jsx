1e3536c4161226b3c5b1280f7e8813f4a4e6bbcb
anyefremova <anna.efremova.02@yandex.ru>
2023-12-31 01:44:59 +0500

    ьade the tariffs interactive, completed the 24 week task

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
+import { useState } from 'react';
 import './App.scss';
 import Tariff from './Tariff/Tariff.jsx';
-import './Tariff/Tariff.scss';
 
 const tariffs = [
-  { id: 1, title: 300, price: 300, speed: 10, color: "blue", select: false },
-  { id: 2, title: 450, price: 450, speed: 50, color: "green", select: false },
-  { id: 3, title: 550, price: 550, speed: 100, color: "red", select: true },
-  { id: 4, title: 1000, price: 1000, speed: 200, color: "black", select: false }
+  { id: 1, title: 300, price: 300, speed: 10, color: "blue" },
+  { id: 2, title: 450, price: 450, speed: 50, color: "green" },
+  { id: 3, title: 550, price: 550, speed: 100, color: "red" },
+  { id: 4, title: 1000, price: 1000, speed: 200, color: "black" }
 ];
 
 function App() {
+
+  const [id, setId] = useState(0);
+
   return (
     <div className="App">
       {tariffs.map((tariff) => (
         <Tariff
           key={tariff.id}
+          currentId={tariff.id}
           title={tariff.title}
           price={tariff.price}
           speed={tariff.speed}
           color={tariff.color}
-          select={tariff.select}
+          activeTarif={tariff.id === id}
+          setId={setId}
         />
       ))}
     </div>
--- src/Tariff/Tariff.jsx
+++ src/Tariff/Tariff.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './Tariff.scss';
 
 function Tariff(props) {
-    const { price, speed, color, select } = props;
-    const tariffClass = `tariff ${color} ${select ? 'selected' : ''}`;
+    const { price, speed, color, activeTarif, currentId, setId } = props;
+    const tariffClass = `tariff ${color} ${activeTarif ? 'activeTarif' : ''}`;
+    function handleActive() {
+        setId(currentId);
+    }
 
     return (
-        <div className={tariffClass}>
+        <div className={tariffClass} onClick={handleActive}>
             <h3 className="title">Безлимитный {price}</h3>
             <p className="price">руб {price} /мес</p>
             <p className="speed">до {speed} Мбит/сек</p>
